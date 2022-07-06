65625b1eeb57222429970b89566c9c55cf9949f3
raviteja <raviteja.venkatesh@costbo.com>
2022-07-06 16:51:52 +0530

    Checkbox list example react js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
 function App() {
+  const [checkedList, setCheckedList] = useState([]);
+  const listData = [
+    { id: "1", value: "Javascript" },
+    { id: "2", value: "Python" },
+    { id: "3", value: "Java" },
+    { id: "4", value: "Kotlin" },
+    { id: "5", value: "Dart" },
+    { id: "6", value: "C#" },
+  ];
+
+  const handleSelect = (event) => {
+    const value = event.target.value;
+    const isChecked = event.target.checked;
+
+    if (isChecked) {
+      //Add checked item into checkList
+      setCheckedList([...checkedList, value]);
+    } else {
+      //Remove unchecked item from checkList
+      const filteredList = checkedList.filter((item) => item !== value);
+      setCheckedList(filteredList);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <div className="card-header">
+          <p className="title">Select Programming Language</p>
+        </div>
+
+        <div className="list-container">
+          <label>You Selected:</label>
+          {checkedList.map((item, index) => {
+            return (
+              <div className="chip">
+                <p className="chip-label">{item}</p>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="card-body">
+          {listData.map((item, index) => {
+            return (
+              <div key={item.id} className="checkbox-container">
+                <input
+                  type="checkbox"
+                  name="languages"
+                  value={item.value}
+                  onChange={handleSelect}
+                />
+                <label>{item.value}</label>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
